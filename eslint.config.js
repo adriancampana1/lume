13b55a2dc57c5326d4ce100c6688032d67d7ac0e
Adrian Campana <adrianphcampana12@gmail.com>
2026-05-02 19:43:55 -0300

    chore: add ESLint flat config and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked.map((config) => ({
+    ...config,
+    files: ['**/*.{ts,tsx}'],
+  })),
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+  {
+    ignores: ['**/dist/', '**/.next/', '**/node_modules/', '**/coverage/', '**/migrations/'],
+  },
+  prettier,
+);
